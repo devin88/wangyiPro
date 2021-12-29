fd672eb07ff99d7f1078cd36697e5268e512e8cd
jidi <jidi@eschervr.com>
2021-12-29 11:37:42 +0800

    wangyi.py #test3

--- wangyiPro/spiders/wangyi.py
+++ wangyiPro/spiders/wangyi.py
@@ -3,6 +3,7 @@ from wangyiPro.items import WangyiproItem
 from selenium import webdriver
 #dfdfdfdf
 #test2
+#test3
 class WangyiSpider(scrapy.Spider):
     name = 'wangyi'
     #allowed_domains = ['www.163.com']
