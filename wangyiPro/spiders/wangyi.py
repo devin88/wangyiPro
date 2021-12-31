411fc08d3351e243c8af61a668c2743053cf4881
jidi <jidi@eschervr.com>
2021-12-31 08:10:59 +0800

    test eastmoney data

--- wangyiPro/spiders/wangyi.py
+++ wangyiPro/spiders/wangyi.py
@@ -10,7 +10,7 @@ import sys,os
 class WangyiSpider(scrapy.Spider):
     name = 'wangyi'
     #allowed_domains = ['www.163.com']
-    start_urls = ['http://news.163.com']
+    start_urls = ['https://push2.eastmoney.com/api/qt/ulist.np/get?fltt=2&invt=2&fields=f2,f3,f12,f14,f9&cb=jQuery183049534260854690526_1640907252730&ut=267f9ad526dbe6b0262ab19316f5a25b&secids=1.688200,0.300450,1.601225,1.600438,0.300037,0.300390,1.603906,1.600188,1.603260,0.002756,&_=1640907253735']
     model_urls = []
     osversion=platform.system()
     project_path = os.path.dirname(os.path.abspath(__file__))
@@ -26,6 +26,18 @@ class WangyiSpider(scrapy.Spider):
     else:
         sys.exit()
 
+    def parse(self,response):
+        #urls = ['http://fundf10.eastmoney.com/ccmx_006230.html']
+        for url in self.start_urls:
+            yield scrapy.Request(url=url,callback=self.parse_check)
+
+    def parse_check(self,response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
+'''
     def parse(self, response):
         li_list = response.xpath('//*[@id="index2016_wrap"]/div[1]/div[2]/div[2]/div[2]/div[2]/div/ul/li')
         index = [2,3,5,6,8]
@@ -56,6 +68,6 @@ class WangyiSpider(scrapy.Spider):
 
     def closed(self,reason):
         self.driver.quit()
-
+'''
 
 
